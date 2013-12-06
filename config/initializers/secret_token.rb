861663ef51333693ebbc2e79bb2d534634ddfe7d
Ruby <ruby@ruby-virtual-machine.(none)>
2013-12-06 10:13:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeezeMs::Application.config.secret_key_base = '12eac4929c264b70fe9827e1e3e416c70a0e0a200b7463062f42ac92dde5deb5a462dd0a79b6a2c8d14fa5779fcf7a504e3777f3e9cf452f96a46e8050a0fa65'
